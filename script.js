e513a5494f4e8d69018a54ef39b2b29cb4cec4d0
Zoltán Halmay <132577193+zoltanhalmay1984@users.noreply.github.com>
2023-09-13 11:49:59 +0200

    Creating obstacles is done

--- script.js
+++ script.js
@@ -37,9 +37,9 @@ window.addEventListener('load', function () {
             this.dx = this.game.mouse.x - this.collisionX;
             this.dy = this.game.mouse.y - this.collisionY;
             const distance = Math.hypot(this.dy, this.dx);
-            if (distance > this.speedModifier){
-            this.speedX = this.dx / distance || 0;
-            this.speedY = this.dy / distance || 0;
+            if (distance > this.speedModifier) {
+                this.speedX = this.dx / distance || 0;
+                this.speedY = this.dy / distance || 0;
             } else {
                 this.speedX = 0;
                 this.speedY = 0;
@@ -49,12 +49,32 @@ window.addEventListener('load', function () {
         }
     }
 
+    class Obstacle {
+        constructor(game) {
+            this.game = game;
+            this.collisionX = Math.random() * this.game.width;
+            this.collisionY = Math.random() * this.game.height;
+            this.collisionRadius = 100;
+        }
+        draw(context) {
+            context.beginPath();
+            context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+            context.save();
+            context.globalAlpha = 0.5;
+            context.fill();
+            context.restore();
+            context.stroke();
+            context.beginPath();
+        }
+    }
     class Game {
         constructor(canvas) {
             this.canvas = canvas;
             this.width = this.canvas.width;
             this.height = this.canvas.height;
             this.player = new Player(this);
+            this.numberOfObstacles = 5;
+            this.obstacles = [];
             this.mouse = {
                 x: this.width * 0.5,
                 y: this.height * 0.5,
@@ -81,10 +101,18 @@ window.addEventListener('load', function () {
         render(context) {
             this.player.draw(context);
             this.player.update();
+            this.obstacles.forEach(obstacle => obstacle.draw(context));
+        }
+        init() { //initialize
+            for (let i = 0; i < this.numberOfObstacles; i++) { //it creates 5 obstacles
+                this.obstacles.push(new Obstacle(this));
+            }
         }
     }
 
     const game = new Game(canvas);
+    game.init();
+    console.log(game);
 
     function animate() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
